22193b1e2a90f665957a7089130c7be2f3897d27
haoyuan.hhy <haoyuan.hhy@alibaba-inc.com>
2022-04-26 15:02:36 +0800

    init

---
+++ README.md
@@ -0,0 +1,27 @@
+## [React-2048](https://kwrush.github.io/react-2048/)
+
+A React implementation of [2048](https://github.com/gabrielecirulli/2048) built with [Typescript](https://www.typescriptlang.org/) and 💅 [styled-components](https://styled-components.com)
+
+### Demo
+
+Try online demo [here](https://kwrush.github.io/react-2048/)
+
+### Getting Started
+
+```shell
+$ npm install
+$ npm start
+# open http://localhost:3000
+```
+
+You can also access the local development server from mobile
+
+```shell
+$ npm install
+$ npm run start:host
+# open http://${your_computer_ip}:3000, for exmaple http://192.168.0.1:3000
+```
+
+### Liscense
+
+MIT
---
+++ src/App/App.tsx
@@ -0,0 +1,149 @@
+import React, { FC, useCallback, useEffect } from 'react';
+import { ThemeProvider } from 'styled-components';
+import Box from '../components/Box';
+import Control from '../components/Control/Control';
+import GameBoard from '../components/GameBoard';
+import ScoreBoard from '../components/ScoreBoard';
+import Switch from '../components/Switch';
+import Text from '../components/Text';
+import useGameBoard from '../hooks/useGameBoard';
+import useGameScore from '../hooks/useGameScore';
+import useGameState, { GameStatus } from '../hooks/useGameState';
+import useScaleControl from '../hooks/useScaleControl';
+import { GRID_SIZE, MIN_SCALE, SPACING } from '../utils/constants';
+import useLocalStorage from '../hooks/useLocalStorage';
+import { ThemeName } from '../themes/types';
+import useTheme from '../hooks/useTheme';
+
+export type Configuration = {
+  theme: ThemeName;
+  bestScore: number;
+  rows: number;
+  cols: number;
+};
+
+const APP_NAME = 'react-2048';
+
+const App: FC = () => {
+  const [{ status: gameStatus, pause }, setGameStatus] = useGameState({
+    status: 'running',
+    pause: false,
+  });
+
+  const [config, setConfig] = useLocalStorage<Configuration>(APP_NAME, {
+    theme: 'default',
+    bestScore: 0,
+    rows: MIN_SCALE,
+    cols: MIN_SCALE,
+  });
+
+  const [{ name: themeName, value: themeValue }, setTheme] = useTheme(
+    config.theme,
+  );
+
+  const [rows, setRows] = useScaleControl(config.rows);
+  const [cols, setCols] = useScaleControl(config.cols);
+
+  const { total, best, addScore, setTotal } = useGameScore(config.bestScore);
+
+  const { tiles, onMove, onMovePending } = useGameBoard({
+    rows,
+    cols,
+    pause,
+    gameStatus,
+    setGameStatus,
+    addScore,
+  });
+
+  const onResetGame = useCallback(() => {
+    setGameStatus('restart');
+  }, [setGameStatus]);
+
+  const onCloseNotification = useCallback(
+    (currentStatus: GameStatus) => {
+      setGameStatus(currentStatus === 'win' ? 'continue' : 'restart');
+    },
+    [setGameStatus],
+  );
+
+  useEffect(() => {
+    if (gameStatus === 'restart') setTotal(0);
+  }, [gameStatus, setTotal]);
+
+  useEffect(() => {
+    setConfig({ rows, cols, bestScore: best, theme: themeName });
+  }, [rows, cols, best, themeName, setConfig]);
+
+  return (
+    <ThemeProvider theme={themeValue}>
+      <Box
+        justifyContent="center"
+        inlineSize="100%"
+        blockSize="100%"
+        alignItems="start"
+        borderRadius={0}
+      >
+        <Box
+          justifyContent="center"
+          flexDirection="column"
+          inlineSize={`${GRID_SIZE}px`}
+        >
+          <Box marginBlockStart="s6" inlineSize="100%" justifyContent="end">
+            <Switch
+              title="dark mode"
+              checked={themeName === 'dark'}
+              activeValue="dark"
+              inactiveValue="default"
+              onChange={setTheme}
+            />
+          </Box>
+          <Box
+            inlineSize="100%"
+            justifyContent="space-between"
+            marginBlockStart="s2"
+          >
+            <Box>
+              <Text fontSize={64} fontWeight="bold" color="primary">
+                2048
+              </Text>
+            </Box>
+            <Box justifyContent="center">
+              <ScoreBoard total={total} title="score" />
+              <ScoreBoard total={best} title="best" />
+            </Box>
+          </Box>
+          <Box marginBlockStart="s2" marginBlockEnd="s6" inlineSize="100%">
+            <Control
+              rows={rows}
+              cols={cols}
+              onReset={onResetGame}
+              onChangeRow={setRows}
+              onChangeCol={setCols}
+            />
+          </Box>
+          <GameBoard
+            tiles={tiles}
+            boardSize={GRID_SIZE}
+            rows={rows}
+            cols={cols}
+            spacing={SPACING}
+            gameStatus={gameStatus}
+            onMove={onMove}
+            onMovePending={onMovePending}
+            onCloseNotification={onCloseNotification}
+          />
+          <Box marginBlock="s4" justifyContent="center" flexDirection="column">
+            <Text fontSize={16} as="p" color="primary">
+              ✨ Join tiles with the same value to get 2048
+            </Text>
+            <Text fontSize={16} as="p" color="primary">
+              🕹️ Play with arrow keys or swipe
+            </Text>
+          </Box>
+        </Box>
+      </Box>
+    </ThemeProvider>
+  );
+};
+
+export default App;
